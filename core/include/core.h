50565d302c176f7f07b10530b1a8f114df6102f4
BasJansw <bas.jansweijer@uva.student.nl>
2025-02-13 22:13:18 +0100

    fix: remove example code

--- core/include/core.h
+++
@@ -1,8 +0,0 @@
-#ifndef CORE_H
-#define CORE_H
-
-#include "math_utils.h"
-
-void core_function();
-
-#endif // CORE_H
--- core/include/math_utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
-
-int add(int a, int b);
-double multiply(double a, double b);
-
-#endif // MATH_UTILS_H
