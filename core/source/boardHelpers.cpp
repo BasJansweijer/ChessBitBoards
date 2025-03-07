fccda8ae877932ae5da5022711ab7d56e24c13ce
BasJansw <bas.jansweijer@uva.student.nl>
2025-03-07 11:49:26 +0100

    rework pawn attack detection

--- core/source/boardHelpers.cpp
+++ core/source/boardHelpers.cpp
@@ -4,7 +4,6 @@
 
 namespace chess
 {
-    
 
     bool BoardState::squareAttacked(square s, bool byWhite) const
     {
@@ -12,16 +11,17 @@ namespace chess
         const PieceSet &attacker = byWhite ? m_white : m_black;
 
         bitboard allBlockers = allPieces();
-
-        int8_t rank = s / 8;
         int8_t file = s % 8;
-        int8_t pawnRank = byWhite ? rank - 1 : rank + 1;
 
-        square pawnAttackRight = file != 7 ? pawnRank * 8 + file + 1 : -1;
-        square pawnAttackLeft = file != 0 ? pawnRank * 8 + file - 1 : -1;
+        // compute the square above/bellow square s (on rank from which pawns can attack)
+        square pawnRank = s - (byWhite ? 8 : -8);
+        square pawnAttackRight = pawnRank + 1;
+        square pawnAttackLeft = pawnRank - 1;
 
-        return attacker.pawns & 1ULL << pawnAttackLeft ||
-               attacker.pawns & 1ULL << pawnAttackRight ||
+        // We check if we aren't on the edge of the board since in that case
+        // the pawn attack square doesn't exist
+        return (file != 0 && attacker.pawns & 1ULL << pawnAttackLeft) ||
+               (file != 7 && attacker.pawns & 1ULL << pawnAttackRight) ||
                constants::knightMoves[s] & attacker.knights ||
                constants::kingMoves[s] & attacker.king ||
                constants::getBishopMoves(s, allBlockers) & (attacker.bishops | attacker.queens) ||
